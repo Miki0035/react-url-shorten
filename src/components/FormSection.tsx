0d8f83e9998ff52763b8e0a31d7d7d9c39bad0ee
Mikiyas Kebede <mknewkebede@gmail.com>
2025-03-19 16:51:14 +0300

    social media hover left

--- src/components/FormSection.tsx
+++ src/components/FormSection.tsx
@@ -26,13 +26,26 @@ const FormSection = () => {
   function generateShortKey() {
     return crypto.getRandomValues(new Uint32Array(1))[0].toString(36);
   }
+
+  function copyToClipBoard(index: number, e: React.MouseEvent) {
+    // Change Text , Background Color of Button to Copied
+    (e.target as HTMLButtonElement).textContent = "Copied!";
+    (e.target as HTMLButtonElement).style.backgroundColor = "hsl(260, 8%, 14%)";
+
+
+    const copyText = document.querySelector(`.link-${index}`);
+    navigator.clipboard.writeText(copyText!.textContent!);
+  }
   return (
     <section className="form-section">
       <div className="form-container">
         <form onSubmit={handleSubmit}>
           <div className="input-container">
             <input
-              style={{ borderColor: error !== "" ? "hsl(0, 87%, 67%)" : "" }}
+              style={{
+                borderColor: error !== "" ? "hsl(0, 87%, 67%)" : "",
+                borderWidth: error !== "" ? "3px " : "",
+              }}
               type="text"
               name="text"
               id="text"
@@ -52,8 +65,8 @@ const FormSection = () => {
           <div key={index} className="shortened-link-container">
             <div className="original-link">{linkText}</div>
             <div className="shortened-link">
-              <p>{link}</p>
-              <button>copy</button>
+              <p className={`link-${index}`}>{link}</p>
+              <button onClick={(e) => copyToClipBoard(index, e)}>copy</button>
             </div>
           </div>
         ))}
