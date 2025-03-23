26c1283a5fdd59026fd856d3d154453aafac58f4
Mikiyas Kebede <mknewkebede@gmail.com>
2025-03-23 15:35:33 +0300

    secure false proxy

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api/shorten': {
         target: 'https://cleanuri.com/api/v1/shorten',
         changeOrigin: true,
-        secure: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api\/shorten/, ''),
       },
     },
